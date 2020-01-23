ae11ec529030051ae75f3960e2599681c10f59cb
brianetaveras <taveras574@gmail.com>
2020-01-22 21:05:41 -0500

    Added source code link to the bottom

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer(){
     return (
         <div className="footer">
-            Built with ReactJS
+            Built with ReactJS. <a href="https://github.com/brianetaveras/portfolio">Source code</a>
         </div>
     )
 }
