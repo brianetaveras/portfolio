2ba59054d63d75b6a798f8332e39e0e79aa8d686
brianetaveras <taveras574@gmail.com>
2020-01-25 12:48:50 -0500

    Fixed hover color for the navigation icons

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -12,9 +12,9 @@ export default function Navigation(){
                     <div className="content">
                         <div className="logo"><Link to="/">BRIAN TAVERAS</Link></div>
                         <nav className="links">
-                            <a title="Github link" href="https://github.com/brianetaveras/"><i className="fa fa-github-square"></i></a>
-                            <a title="Linked link" href="https://www.linkedin.com/in/brianetaveras/"><i className="fa fa-linkedin-square"></i></a>
-                            <a title="Email link" href="mailto:taveras574@gmail.com"><i className="fa fa-at"></i></a>
+                            <a target="_blank" title="Github link" href="https://github.com/brianetaveras/"><i className="fa fa-github-square"></i></a>
+                            <a target="_blank" title="Linked link" href="https://www.linkedin.com/in/brianetaveras/"><i className="fa fa-linkedin-square"></i></a>
+                            <a target="_blank" title="Email link" href="mailto:taveras574@gmail.com"><i className="fa fa-at"></i></a>
                         </nav>
                     </div>
                 </Container>
