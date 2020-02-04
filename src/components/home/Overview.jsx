588c57da79dcc50b28f0387f695daf48466ee227
brianetaveras <taveras574@gmail.com>
2020-02-04 13:10:39 -0500

    Some changes to the overview copy

--- src/components/home/Overview.jsx
+++ src/components/home/Overview.jsx
@@ -6,10 +6,10 @@ export default function Overview(){
             <h1>Overview</h1>
             <div className="text">
 
-            I'm a Front-end Web Developer with Trust & Safety and Design background. 
+            I'm a Front-end Web Developer with a Trust & Safety and Design background. 
 <br/>
 <br/>
-My skill repository includes modern Javascript frameworks such as VueJS and ReactJS. I'm also fluent in modern CSS, CSS Animations, and pre-processors such a Less and Sass.
+My skill repository includes modern Javascript frameworks such as VueJS and ReactJS. I'm also fluent in modern CSS, CSS Animations, and pre-processors like Less and Sass.
             </div>
         </div>
     )
