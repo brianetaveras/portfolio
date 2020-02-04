a9b68df1229af4b74e439ad1737a9fcfead8bbc6
brianetaveras <taveras574@gmail.com>
2020-02-04 10:24:20 -0500

    Added overview text

--- src/components/home/Overview.jsx
+++ src/components/home/Overview.jsx
@@ -5,7 +5,11 @@ export default function Overview(){
         <div className="overview">
             <h1>Overview</h1>
             <div className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+
+            I'm a Front-end Web Developer with Trust & Safety and Design background. 
+<br/>
+<br/>
+My skill repository includes modern Javascript frameworks such as VueJS and ReactJS. I'm also fluent in modern CSS, CSS Animations, and pre-processors such a Less and Sass.
             </div>
         </div>
     )
